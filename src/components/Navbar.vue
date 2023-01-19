1eb4428916e940da8d73ea3cefc01495badc0a82
Luis Gustavo <gustavobarbosag7@gmail.com>
2023-01-18 21:03:02 -0300

    implementing vue router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img id="logo" src="">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">About</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
