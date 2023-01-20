f7bd747a0746a5c9142da2f1d1e6abff397bb322
Luis Gustavo <gustavobarbosag7@gmail.com>
2023-01-20 19:44:26 -0300

    header and footer styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <Navbar />
+  <Navbar
+    :logo="logo_src"
+    :alt="app_name"
+  />
   <router-view/>
   <Footer />
 </template>
@@ -12,11 +15,34 @@
     components: {
       Navbar,
       Footer
+    },
+    data() {
+      return {
+        logo_src: '/img/logo.png',
+        app_name: 'Make Your Burguer'
+      }
     }
   }
 </script>
 
 
 <style>
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: Helvetica;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    margin-bottom: 30px;
+    font-size: 42px;
+    color: #222;
+    text-align: center;
+  }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,48 @@
 <template>
   <nav>
-    <router-link to="/">
-      <img id="logo" src="">
+    <router-link to="/" id="logo-url">
+      <img id="logo" :src="logo" :alt="alt">
     </router-link>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/pedidos">About</router-link>
   </nav>
 </template>
 
 <script>
   export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    props: ['logo', 'alt']
   }
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: flex-end;
+    align-content: center;
+    background: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+  }
 
+  #logo-url {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  nav a {
+    margin: 12px;
+    color: #fcba03;
+    text-decoration: none;
+    transition: .5s;
+  }
+
+  nav a:hover {
+    color: #fff;
+  }
 </style>
